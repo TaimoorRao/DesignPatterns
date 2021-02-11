a48a8192b5857a86dcaaec46bb56e305ed78c9d5
Taimoor Khalid <taimoor.rao12@gmail.com>
2021-02-11 12:51:28 +0500

    Singleton Pattern Example

---
+++ SingletonPatternExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTon obj1 = SingleTon.MyObject();
+            obj1.Method();
+            Console.ReadLine();
+        }
+    }
+    class SingleTon
+    {
+        private SingleTon(){ }
+        public static SingleTon getInstance = null;
+        private static readonly object syncThread = new object();
+        public static SingleTon MyObject()
+        {
+            if (getInstance == null)
+            {
+                lock (syncThread)
+                    if (getInstance == null)
+                        return new SingleTon();
+            }
+            return getInstance;
+        }
+        public void Method()
+        {
+            Console.WriteLine("This is my method");
+        }
+    }
+}
